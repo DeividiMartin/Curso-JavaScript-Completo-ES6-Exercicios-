bf1b57911904e08d206c03abfcb3dca1d1d6bb9c
Deividi Martin <deividi_97@hotmail.com>
2025-02-14 19:05:05 -0300

    exercicios de 1 a 10 java iniciante

---
+++ 1-Variaveis/script.js
@@ -0,0 +1,16 @@
+//Declarar uma variável com seu nome
+let nome = "Deividi Martin Ribeiro";
+
+//Declarar uma variável com sua idade
+let idade = 27;
+
+//Declarar uma variável com sua comida favorita e não atribuir valor
+let comidaFavorita;
+
+//Atribuir valor a sua comida favorita
+comidaFavorita = "pizza";
+
+//Declarar 5 variaveis diferente sem valor
+let time, pais, endereco, cidade, sexo;
+
+console.log(nome, idade, comidaFavorita, time, pais, endereco, cidade, sexo);
---
+++ 2-Tipos-de-Dados/script.js
@@ -0,0 +1,21 @@
+// Declare uma variável contendo uma string
+let nome = "Deividi";
+
+// Declare uma variável contendo um número dentro de uma string
+let ano = "2025";
+
+// Declare uma variável com a sua idade
+let idade = 27;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let sobrenome = "Martin";
+let nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma vartiável: It's time
+let frase = `It's time`;
+
+// Verifique o tipo da variável que contém o seu nome
+let verificarTipoNome = typeof nome;
+
+console.log(verificarTipoNome);
