708ef7eedfafaf5d9991b2a5fd42fe79cd7148df
Deividi Martin <deividi_97@hotmail.com>
2025-02-22 20:25:41 -0300

    prototype

--- 3-Objetos/1-Constructor/script.js
+++ 3-Objetos/1-Constructor/script.js
@@ -1,52 +1,49 @@
 // Transforme o objeto abaixo em uma Constructor Function
 const pessoa = {
-  nome: 'Nome pessoa',
+  nome: "Nome pessoa",
   idade: 0,
   andar() {
-    console.log(this.nome + ' andou');
-  }
-}
+    console.log(this.nome + " andou");
+  },
+};
 
 function Pessoa(nome, idade) {
   this.nome = nome;
   this.idade = idade;
-  this.andar = function() {
-    console.log(nome + ' Andou');
-  }
+  this.andar = function () {
+    console.log(nome + " andou");
+  };
 }
 
 // Crie 3 pessoas, João - 20 anos,
 // Maria - 25 anos, Bruno - 15 anos
-const joao = new Pessoa('João', 20);
-const maria = new Pessoa('Maria', 25);
-const bruno = new Pessoa('Bruno', 15);
-
+const pessoa1 = new Pessoa("João", 20);
+const pessoa2 = new Pessoa("Maria", 20);
+const pessoa3 = new Pessoa("Bruno", 15);
 
 // Crie uma Constructor Function (Dom) para manipulação
 // de listas de elementos do dom. Deve conter as seguintes
 // propriedades e métodos:
-//
+function Dom(selector) {
+  const elementList = document.querySelectorAll(selector);
+  this.elementList = elementList;
+  this.addClass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.add(classe);
+    });
+  };
+  this.removeClass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.remove(classe);
+    });
+  };
+}
+
+const listaItens = new Dom("li");
+
 // elements, retorna NodeList com os elementos selecionados
 // addClass(classe), adiciona a classe a todos os elementos
 // removeClass(classe), remove a classe a todos os elementos
-
-function Dom(seletor) {
-  const elementList = document.querySelectorAll(seletor);
-  this.elements = elementList;
-  this.addClass = function(classe) {
-    elementList.forEach((element) => {
-      element.classList.add(classe);
-    })
-  }
-  this.removeClass = function(classe) {
-    elementList.forEach((element) => {
-      element.classList.remove(classe);
-    })
-  }
-}
-
-const listaItens = new Dom('li');
-const ul = new Dom('ul');
 
 // listaItens.addClass('ativar');
 // ul.addClass('ativar-ul')
