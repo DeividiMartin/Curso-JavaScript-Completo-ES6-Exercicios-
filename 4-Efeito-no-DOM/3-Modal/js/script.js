e4801d58c87ff591a4ef25fc5f35d1b57afaf5a4
Deividi Martin <deividi_97@hotmail.com>
2025-03-01 22:32:18 -0300

    Tooltip

--- 4-Efeito-no-DOM/3-Modal/js/script.js
+++ 4-Efeito-no-DOM/3-Modal/js/script.js
@@ -1,8 +1,8 @@
-import initScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initAccordion from './modules/accordion.js';
-import initTabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
 initScrollSuave();
 initAnimacaoScroll();
