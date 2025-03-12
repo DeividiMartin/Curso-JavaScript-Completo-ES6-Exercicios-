d3ce27a7e913bfcbeaea104ec966fbcfd7192f1e
Deividi Martin <deividi_97@hotmail.com>
2025-03-11 22:09:22 -0300

    Promisses

---
+++ 5-JavaScript-Assíncrono/3-Fatch/script.js
@@ -0,0 +1,10 @@
+const doc = fetch('./doc.txt');
+
+doc
+  .then((resolucao) => {
+    return resolucao.text();
+  })
+  .then((a) => {
+    const conteudo = document.querySelector('.conteudo');
+    conteudo.innerText = a;
+  });
